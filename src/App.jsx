786c605c739f2d0fc8ccffef42500098257e2468
bjisadorozco <bjisadorozco@unicesar.edu.co>
2024-03-21 17:50:35 -0500

    Formularios de Iniciar sesion y Registrarse

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AuthForm from "./AuthForm";
 
 function App() {
   return (
-    
+    <div>
+      <AuthForm />
+    </div>
   );
 }
 
